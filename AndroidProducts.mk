ee1d940336dd69c747413d458288a4ed5adfbb15
opsonc <nbodnarandreea@gmail.com>
2023-08-12 11:34:06 +0300

    added files
    meower here :33

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_IRM_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_IRM_sprout-user \
+    omni_IRM_sprout-userdebug \
+    omni_IRM_sprout-eng
---
+++ omni_IRM_sprout.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from IRM_sprout device
+$(call inherit-product, device/hmd/IRM_sprout/device.mk)
+
+PRODUCT_DEVICE := IRM_sprout
+PRODUCT_NAME := omni_IRM_sprout
+PRODUCT_BRAND := Nokia
+PRODUCT_MODEL := Nokia 2.3
+PRODUCT_MANUFACTURER := hmd
+
+PRODUCT_GMS_CLIENTID_BASE := android-hmd-rev2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_T99621AA2-user 10 QP1A.190711.020 p1k61v164bspP56 release-keys"
+
+BUILD_FINGERPRINT := Nokia/Ironman_00WW/IRM_sprout:10/QP1A.190711.020/00WW_2_330:user/release-keys
